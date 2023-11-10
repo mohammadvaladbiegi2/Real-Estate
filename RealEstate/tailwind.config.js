b9259e993800f5487a66f80822dcdb9fe6f4d9b7
◇HemîD◇ <124708686+Hamid061002@users.noreply.github.com>
2023-11-10 22:02:27 +0330

    color tailwind

--- RealEstate/tailwind.config.js
+++ RealEstate/tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors:{
+    'color1': '#6D31D0',
+    'color2': '#828CDE',
+    'color3': '#FFDB80',
+    'color4': '#FDBFA3',
+    'color5': '#53269D',
+    'color6': '#FFF',
+    'yelow': '#FFEB80',
+    'block' : '#434364',
+    },
   },
   plugins: [],
 };
