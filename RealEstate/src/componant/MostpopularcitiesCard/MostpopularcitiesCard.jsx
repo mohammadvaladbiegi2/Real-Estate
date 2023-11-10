6aff0f43ddc42c31e43fb4541a33443499e562c4
mohammad <mohammadvaladbiegi2@gmail.com>
2023-11-10 22:00:59 +0330

    componantMV

--- RealEstate/src/componant/MostpopularcitiesCard/MostpopularcitiesCard.jsx
+++ RealEstate/src/componant/MostpopularcitiesCard/MostpopularcitiesCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./MostpopularcitiesCard.css";
 export default function MostpopularcitiesCard() {
-  return <div>MostpopularcitiesCard</div>;
+  return (
+    <>
+      <div className="main-card bg-slate-100">
+        <img src="../public/assets/images/offer_cities_slider1_image.png" />
+        <span className="main-text">Nicosia</span>
+      </div>
+    </>
+  );
 }
