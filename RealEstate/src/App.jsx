c1cf40111135da42c995530d38a4910d588e7387
◇HemîD◇ <124708686+Hamid061002@users.noreply.github.com>
2023-11-17 23:35:13 +0330

    feat: business relocation created

--- RealEstate/src/App.jsx
+++ RealEstate/src/App.jsx
@@ -4,13 +4,14 @@ import routs from "./routs";
 import Navbar from "./componant/Navbar/Navbar";
 import ContactUs from "./componant/ContactUs/ContactUs";
 import About from "./page/About/About";
-import Migration from "./page/Migration/Migration";
+// import Migration from "./page/Migration/Migration";
 import Homepage from "./page/Homepage/Homepage";
+import BusinessRelocation from "./page/BusinessRelocation/BusinessRelocation";
 
 function App() {
   let rout = useRoutes(routs);
   return <>
-
+  <BusinessRelocation/>
   {rout}</>;
 }
 
